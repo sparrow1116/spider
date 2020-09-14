b04b584f31cf5c43e234c702f82145b2df7bc2e4
sparrow1116 <yingjiez1@126.com>
2020-09-15 07:41:35 +0800

    zyj commit bank detail last modify

--- src/push/pushBankDailyDetail.js
+++ src/push/pushBankDailyDetail.js
@@ -1,26 +1,29 @@
+/**
+ * Created by zyj on 2020/9/14.
+ */
 const fetch = require('node-fetch');
 const BaseUrl = require('../config').serverUrl
 const fs = require('fs')
 
-var data=fs.readFileSync('../wankahaoyangmao/result.json',"utf8")
+var data=fs.readFileSync('../wankahaoyangmao/detailArr.json',"utf8")
 console.log('readfs data::')
-fetch(BaseUrl+'/api/saveBankList',{
+fetch(BaseUrl+'/api/saveBankDetailList',{
     method: "POST",
     credentials: 'include',
     headers: {
-        "Content-Type": "application/json",
+	"Content-Type": "application/json",
     },
     body: data,
-    
+
 }).then(function(response) {
-    
+
     return response.json();
-  }).then(
+}).then(
     function(myBlob){
-        console.log(myBlob);
+	console.log(myBlob);
     }
 ).catch(
     function(error){
-        console.log(error)
+	console.log(error)
     }
 )
